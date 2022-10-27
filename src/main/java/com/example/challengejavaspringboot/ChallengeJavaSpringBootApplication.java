8137440b87187d9ec32f66e6889f1a03b2b0c49c
sbensarg <sarabensarghin9@gmail.com>
2022-10-27 23:32:48 +0100

    init

---
+++ src/main/java/com/example/challengejavaspringboot/ChallengeJavaSpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.example.challengejavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeJavaSpringBootApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ChallengeJavaSpringBootApplication.class, args);
+        String dv = "ajhdfa";
+        int hrlo = 5;
+
+    }
+
+
+
+}
---
+++ src/test/java/com/example/challengejavaspringboot/ChallengeJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengejavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
