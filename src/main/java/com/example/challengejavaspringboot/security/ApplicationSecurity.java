e438ac23ea6076ebd659446e3f148a506232a2dc
sbensarg <sarabensarghin9@gmail.com>
2022-11-06 01:04:21 +0100

    fix WebSecurityConfigurerAdapter deprecated in Spring Boot

--- src/main/java/com/example/challengejavaspringboot/security/ApplicationSecurity.java
+++ src/main/java/com/example/challengejavaspringboot/security/ApplicationSecurity.java
@@ -1,25 +1,26 @@
 package com.example.challengejavaspringboot.security;
+import com.example.challengejavaspringboot.services.JwtUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import javax.servlet.http.HttpServletResponse;
 
-@EnableWebSecurity
+@Configuration
 @EnableGlobalMethodSecurity(
-        prePostEnabled = true, securedEnabled = true, jsr250Enabled = true
+        prePostEnabled = true
 )
 
-public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+public class ApplicationSecurity {
 
     @Autowired
     private JwtTokenFilter jwtTokenFilter;
@@ -35,10 +36,15 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
             "/api/users/batch",
             "/api/auth"
     };
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(
-                username -> jwtUserDetailsService.loadUserByUsername(username));
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(jwtUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
     }
 
     @Bean
@@ -46,8 +52,14 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
         http.cors().and().csrf().disable();
         http.headers().frameOptions().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -67,12 +79,10 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
                         }
                 );
 
-        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-    }
+        http.authenticationProvider(authenticationProvider());
 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
